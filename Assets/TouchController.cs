664f8efa1de529177907db49f52585db5a176f32
Dmitriy Skladnov <muaitaibox@gmail.com>
2020-06-20 19:40:02 +0300

    hide title on tap

---
+++ Assets/TouchController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+	public GameObject[] toggleableObjects;
+
+    void Start()
+    {
+    	toggleableObjects = GameObject.FindGameObjectsWithTag("Toggleable");    
+    }
+
+    void Update()
+    {
+   		if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Pressed primary button.");
+            foreach(GameObject o in toggleableObjects) {
+				toggleVisibility(o);
+			}
+   		}
+     	
+    }
+
+    private void toggleVisibility(GameObject gameObject) {
+    	if(gameObject.activeSelf == true) {
+    		hide(gameObject);	
+    	} else {
+    		show(gameObject);
+    	}
+    }
+
+    private void show(GameObject gameObject) {
+     	gameObject.SetActive(true);
+ 	}
+ 	
+ 	private void hide(GameObject gameObject) {
+     	gameObject.SetActive(false);
+ 	}
+
+}
+
