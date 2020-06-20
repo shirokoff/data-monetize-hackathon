0de6aa39bf3c3a84dcef2359b013e5a03f831e33
Ivan Shirokov <shirokoff@ya.ru>
2020-06-20 16:23:45 +0300

    Added occlusion

---
+++ Assets/Satellite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Satellite : MonoBehaviour
+{
+    public GameObject center;
+    public float angle;
+    public float initianRotation;
+
+    public float rotationSpeed = 0.5f;
+    public float selfRotationSpeed = 0.5f;
+
+
+    void Start()
+    {
+        double a = angle * Math.PI / 180;
+        Vector3 rotationAxis = new Vector3((float)Math.Sin(a), (float)Math.Cos(a), 0);
+        this.transform.RotateAround(center.transform.position, rotationAxis.normalized, initianRotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        double a = angle * Math.PI / 180;
+        Vector3 rotationAxis = new Vector3((float)Math.Sin(a), (float)Math.Cos(a), 0);
+        this.transform.RotateAround(center.transform.position, rotationAxis.normalized, rotationSpeed);
+        this.transform.Rotate(Vector3.up, -selfRotationSpeed); 
+    }
+}
