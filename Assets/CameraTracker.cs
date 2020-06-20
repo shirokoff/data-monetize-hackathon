8abb21bb3a939390f965e23d7293c93662741558
mcsim <mcsim1993@gmail.com>
2020-06-20 17:24:45 +0300

    camera tracking

---
+++ Assets/CameraTracker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+    public GameObject _camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_camera != null)
+        {
+            this.transform.rotation = _camera.transform.rotation;
+        }
+    }
+}
